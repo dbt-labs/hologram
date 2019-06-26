eb556fb9c27c6758498906600dc65d389a731fdd
Connor McArthur <connor@fishtownanalytics.com>
2019-06-26 14:51:19 -0400

    adds StrEnum, StrLiteral helpers. adds tests for enums.

---
+++ hologram/helpers.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class StrEnum(str, Enum):
+    def __str__(self):
+        return self.value
+
+    # pylint: disable=no-self-argument
+    # The first argument to this function is documented to be the name of the
+    # enum member, not `self`:
+    # https://docs.python.org/3.6/library/enum.html#using-automatic-values
+    def _generate_next_value_(name, *_):
+        return name
+
+
+def StrLiteral(value: str):
+    return StrEnum(value, value)
