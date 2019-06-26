3101234f18fef221f166c64dbe68e9a7d0afe068
Connor McArthur <connor@fishtownanalytics.com>
2019-06-26 14:57:30 -0400

    cleanup arg weirdness

--- hologram/helpers.py
+++ hologram/helpers.py
@@ -5,11 +5,8 @@ class StrEnum(str, Enum):
     def __str__(self):
         return self.value
 
-    # pylint: disable=no-self-argument
-    # The first argument to this function is documented to be the name of the
-    # enum member, not `self`:
     # https://docs.python.org/3.6/library/enum.html#using-automatic-values
-    def _generate_next_value_(name, *_):
+    def _generate_next_value_(name, start, count, last_values):
         return name
 
 
